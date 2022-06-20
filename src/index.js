47d25e0a174b07e99432475d967730840ca01ffe
Jared Fischer <jaredfischer@MacBook-Pro-3.lan>
2022-06-20 15:48:15 +0300

    removed dev tools

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import { Provider } from "react-redux";
 
 const store = createStore(
 	reducers,
-	composeWithDevTools(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	compose(
+		applyMiddleware(thunk)
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
 
