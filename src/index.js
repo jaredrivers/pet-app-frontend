93b4f4b0fbc51dcc008b8b93a2a64ad922b6a400
Jared Fischer <jaredfischer@MacBook-Pro-3.lan>
2022-06-20 15:54:11 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 import { Provider } from "react-redux";
@@ -25,8 +23,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
